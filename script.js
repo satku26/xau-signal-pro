74ac1258eb1eedaf5fe289c9d5425375375df30b
satku26 <kusmaedisatria@gmail.com>
2026-07-05 19:06:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 async function loadPrice() {
-    const url =
-        `https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=5min&outputsize=60&apikey=${API_KEY}`;
-
     try {
+        const url = `https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=5min&outputsize=1&apikey=${API_KEY}`;
+
         const response = await fetch(url);
         const data = await response.json();
 
-        if (!data.values) {
-            document.getElementById("price").innerHTML = "Gagal mengambil data";
+        console.log(data);
+
+        if (data.status === "error") {
+            document.getElementById("price").innerHTML = data.message;
             return;
         }
 
-        const price = data.values[0].close;
+        document.getElementById("price").innerHTML =
+            "$ " + data.values[0].close;
 
-        document.getElementById("price").innerHTML = price;
-
-    } catch (e) {
-        document.getElementById("price").innerHTML = "Error";
+    } catch (err) {
+        document.getElementById("price").innerHTML = err.message;
     }
 }
 
 loadPrice();
-setInterval(loadPrice,60000);
