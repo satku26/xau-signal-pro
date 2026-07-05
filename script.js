f718ad917b6eaf3fb04bf5e028d56cd417d2f7d8
satku26 <kusmaedisatria@gmail.com>
2026-07-05 19:01:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.getElementById("price").innerHTML = API_KEY;
+async function loadPrice() {
+    try {
+        const url = `https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=5min&outputsize=1&apikey=${API_KEY}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+
+        if (data.status === "error") {
+            document.getElementById("price").innerHTML = data.message;
+            return;
+        }
+
+        document.getElementById("price").innerHTML =
+            "$ " + data.values[0].close;
+
+    } catch (err) {
+        document.getElementById("price").innerHTML = err.message;
+    }
+}
+
+loadPrice();
